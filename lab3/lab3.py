25499c27819e9d37391647c65885d2470ca7451c
Alina Orkusha <alina.orkusha@gmail.com>
2020-11-02 17:49:52 +0200

    video upload by link possibility added

--- lab3/lab3.py
+++ lab3/lab3.py
@@ -7,6 +7,7 @@ from motion_algos import blockMotion, blockComp
 import skvideo.datasets
 import csv
 import matplotlib.pyplot as plt
+import pafy
 
 VID_NAME = 'video.mp4'
 ENCODED_NAME = f'encoded_{VID_NAME[:-4]}.npz'
@@ -15,12 +16,21 @@ METHODS = ['4SS', '3SS', 'DS']
 metrics = {'4SS': {}, '3SS': {}, 'DS': {}}
 
 
+def get_video_from_youtube(url):
+    video = pafy.new(url)
+    best = video.getbest(preftype="mp4")
+
+    capture = cv2.VideoCapture()
+    capture.open(best.url)
+    return capture
+
+
 def encoder(videodata, method='DS', mbSize=8, p=2, compute_motion_if_exists=False):
     encoded_name = method + '_' + ENCODED_NAME
 
-    if not (compute_motion_if_exists) and os.path.isfile(encoded_name):
+    if not compute_motion_if_exists and os.path.isfile(encoded_name):
         data_calculated = np.load(encoded_name)
-        return (data_calculated['video'], data_calculated['motion'])
+        return data_calculated['video'], data_calculated['motion']
 
     motion = blockMotion(videodata, method=method, mbSize=mbSize, p=p)
     cut_vid = np.delete(videodata, list(range(1, videodata.shape[0], 2)), axis=0)
